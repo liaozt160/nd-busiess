58d1504e7e1a43d2e13905af092dfd8ca799d79a
tank <tank@ylbservices.com>
2019-06-19 17:26:28 +0800

    1, 修复意向企业请求 2，修复个人企业列表

--- app/Models/BusinessAttention.php
+++ app/Models/BusinessAttention.php
@@ -44,7 +44,7 @@ class BusinessAttention extends Model
 
     public static function getListByBuyer($accountId){
         $query = self::from('attention_to_business as t')
-            ->select(['b.title','t.business_id','a.name','t.account_id','s.buyer','t.buyer_id','t.created_at']);
+            ->select(['b.title','b.listing','b.price','b.status','t.business_id','a.name','t.account_id','s.buyer','t.buyer_id','t.created_at']);
         $query->leftjoin('accounts as a','t.account_id','=','a.id')
             ->leftjoin('business as b','t.business_id','=','b.id')
             ->leftjoin('buyer as s','t.buyer_id','=','s.id');
