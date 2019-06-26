970a92b0e564e78a439362ef512a615affff005e
tank <tank@ylbservices.com>
2019-06-26 16:53:49 +0800

    1， add order interface validate the buyer_id

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -99,9 +99,7 @@ class Order extends Model
             ->leftjoin('accounts as b','o.account_id','=','b.id')
             ->leftjoin('buyer as c','o.buyer_id','=','c.id')
             ->whereNull('o.deleted_at');
-        if($accountId === null){
-            $query->where('o.account_id',$accountId);
-        }else{
+        if($accountId !== null){
             $query->where('o.buyer_id',$buyerId);
         }
         $list = $query->paginate(15);
