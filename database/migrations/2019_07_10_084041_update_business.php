9141c565ae25694126406a6a1112ab560803f047
tank <tank@ylbservices.com>
2019-07-10 16:43:09 +0800

    1, 修改business listing term 字段

---
+++ database/migrations/2019_07_10_084041_update_business.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateBusiness extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('business', function (Blueprint $table) {
+            $table->string('lease_term',100)->nullable()->comment('lease term')->change();
+        });
+        Schema::table('business_zh', function (Blueprint $table) {
+            $table->string('lease_term',100)->nullable()->comment('lease term')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
