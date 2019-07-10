dbae9967784b5d3f6a1053b6ae873cd36910def2
tank <tank@ylbservices.com>
2019-07-10 16:47:17 +0800

    1, 修改business listing term 字段

--- database/migrations/2019_07_10_084041_update_business.php
+++ database/migrations/2019_07_10_084041_update_business.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateBusiness extends Migration
+class UpdateBusinessTwo extends Migration
 {
     /**
      * Run the migrations.
